b2209a99037ec3cbd43b1b86f4baf1ad94afe731
luke <luke.jeon@amanda.co.kr>
2021-08-14 23:16:27 +0900

    RIB xcode 템플릿을 이용하여 'Root' 그룹 이하 'Root' RIB 파일들 생성

---
+++ RibTest/RIBs/Root/RootViewController.swift
@@ -0,0 +1,21 @@
+//
+//  RootViewController.swift
+//  RibTest
+//
+//  Created by yongnam jeon on 2021/08/14.
+//
+
+import RIBs
+import RxSwift
+import UIKit
+
+protocol RootPresentableListener: AnyObject {
+    // TODO: Declare properties and methods that the view controller can invoke to perform
+    // business logic, such as signIn(). This protocol is implemented by the corresponding
+    // interactor class.
+}
+
+final class RootViewController: UIViewController, RootPresentable, RootViewControllable {
+
+    weak var listener: RootPresentableListener?
+}
